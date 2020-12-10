d76d6bf8527e633ff2fc416c4c5537e0cef57693
Wai Ho Chen <waiho.chen@student.manchester.ac.uk>
2020-12-10 10:39:46 +0000

    Comp10120 Lab-Steven

--- StevenChen.php
+++ StevenChen.php
@@ -1,15 +1,9 @@
-<?php
-
-print("Hello World");
-
-?>
 <!-- My First Webpage -->
 <!DOCTYPE html>
     <html lang="en">
         <head>
-            <title>My Submission Dates</title>
+            <title>Bargain Share</title>
             <meta charset="UTF-8"> <!--To avoid errors-->
-            <link rel="stylesheet" type="text/css" href="myStyles.css">
         </head>
         <body style="background-color:white;">
           <header>
@@ -18,11 +12,5 @@ print("Hello World");
           <nav>
             <a href="Steven/index.html">Home</a>
             <b>|</b>
-          <footer>
-            <a href="https://twitter.com"><img src="twitter.png"  alt="twitter icon"></a>
-            <a href="https://facebook.com"><img src="facebook.png"  alt="facebook icon"></a>
-            <a href="https://linkedin.com"><img src="linkin.png"  alt="linkedin icon"></a>
-            <a href="https://snapchat.com"><img src="snapchat.png"  alt="snapchat icon"></a>
-          </footer>
         </body>
 </html>
