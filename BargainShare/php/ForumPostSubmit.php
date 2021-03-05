d0c5e73a34c3c1a845557cdff3ff5f68ba62ab01
x68617wc <waiho.chen@student.manchester.ac.uk>
2021-03-05 16:02:07 +0000

    ForumCreatePost Link Fixed

--- BargainShare/php/ForumPostSubmit.php
+++ BargainShare/php/ForumPostSubmit.php
@@ -1,41 +1,35 @@
-<?php
-include "ConnectDb.php";
-$postID = 123;
-$userID = 456;
-$title = $_POST['title'];
-$description = $_POST['description'];
-$upvotes = 1;
-
-if (mysqli_connect_error()) {
- die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-} else {
- $INSERT = "INSERT INTO `ForumPostsDatabase` (`PostID`, `UserID`, `Title`, `PostContent`, `NoOfUpvotes`) VALUES('".$postID."', '".$userID."', '".$title."', '".$description."', '".$upvotes."')";
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 96666919a29a27076c899cfc12a54a68467c1f87
-$records = $conn->query($INSERT);
-if ($records)
-{
-  echo ("Uploaded SUCCESSFULLY");
-}
-else{
-  echo ("Error: " . $conn->error);
-  echo ("$INSERT");
-}
-}
-
-<<<<<<< HEAD
-
- $conn->close();
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Create Post</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="./images/logo.png">
+    <link rel="stylesheet" type="text/css" href="./styles/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./styles/forumStyle.css">
+  </head>
+  <body>
+    <header>
+      <?php include './php/NavBar.php'?>
+    </header>
+	<p>
 
 
-?>
-=======
- 
- $conn->close();
+		<h1>Create new post</h1>
+
+		<form action="./php/ForumPostSubmit.php" method="POST">
+		  <label for="title">Title:</label><br>
+		  <input type="text" id="title" name="title"><br><br>
+
+		  <label for="description">Description:</label><br>
+		  <textarea name="description" cols="40" rows="5"></textarea><br><br>
+
+		  <input type="submit" onClick="alert('Submitting post...')" value="Submit">
+		</form>
+
+		<p>Click the "Submit" button and the form-data will be sent to a page on the
+		server called "ForumPostSubmit.php".</p>
 
 
-?>
->>>>>>> 96666919a29a27076c899cfc12a54a68467c1f87
+	</p>
+  </body>
