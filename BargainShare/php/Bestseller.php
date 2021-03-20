b5163e9291fcb44cbd8838de18a01dfc3cc26d5a
x68617wc <waiho.chen@student.manchester.ac.uk>
2021-03-20 18:22:05 +0000

    Merge Taeseung

---
+++ BargainShare/php/Bestseller.php
@@ -0,0 +1,48 @@
+<?php
+
+	$ProductName = [];
+	$ProductType = [];
+	$ProductPrice = [];
+	$ProductDiscount = [];
+	$ProductValidDate = [];
+	$ProductNoOfUpVote = [];
+	$ProductMessage = [];
+	$ProductImgCount = [];
+	$ProductSource = [];
+	$ProductDate = [];
+	$ProductUserID = [];
+	$Productimage =[];
+	$ProductID =[];
+
+	include './php/ConnectDb.php';
+    $sql = "SELECT * FROM ProductPostsDatabase ORDER BY NoOfUpVotes";
+    $result = $conn->query($sql);
+
+    while($row = $result->fetch_assoc()){
+    	$ProductID[] = $row["PostID"];
+    	$ProductName[] = $row["ItemName"];
+		$ProductType[] = $row["ItemType"];
+		$ProductPrice[] = $row["Price"];
+		$ProductDiscount[] = $row["Discount"];
+		$ProductValidDate[] = $row["DiscountValidDate"];
+		$ProductNoOfUpVote[] = $row["NoOfUpVotes"];
+		$ProductMessage[] = $row["Message"];
+		$ProductImgCount[]  = $row["ImageCount"];
+		$ProductSource[] = $row["Source"];
+		$ProductDate[] =$row["Date"];
+		$ProductUserID[] = $row["UserID"];
+
+
+
+		$image = "SELECT ImageData FROM ImageDatabase WHERE ImageIndex=0 and PostID = ".$row["PostID"];
+		$imageresult = $conn -> query($image);
+		$imagerow = $imageresult->fetch_assoc();
+		$Productimage[] = $imagerow["ImageData"];
+
+    }
+
+
+      $conn->close();
+
+
+?>
